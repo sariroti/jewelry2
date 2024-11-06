b2b62ed8d005b617c0031325c71df401972b1f02
Erwin Suhandi <erwin_suhandi@yahoo.com>
2024-11-06 11:51:48 +0700

    menu changes

--- Jewelry/components/common/header.jsx
+++ Jewelry/components/common/header.jsx
@@ -27,13 +27,46 @@ export default function Header( props ) {
 
     return (
         <header className="header header-border">
-            <div className="header-top">
+            {/* <div className="header-top">
                 <div className="container">
                     <div className="header-left">
-                        <p className="welcome-msg">Welcome to Jewelery store</p>
+                        <p className="welcome-msg ls-normal">Welcome to Riode store message or remove it!</p>
                     </div>
                     <div className="header-right">
                         <div className="dropdown">
+                            <ALink href="#">USD</ALink>
+                            <ul className="dropdown-box">
+                                <li><ALink href="#">USD</ALink></li>
+                                <li><ALink href="#">EUR</ALink></li>
+                            </ul>
+                        </div>
+
+                        <div className="dropdown">
+                            <ALink href="#">ENG</ALink>
+                            <ul className="dropdown-box">
+                                <li>
+                                    <ALink href="#">ENG</ALink>
+                                </li>
+                                <li>
+                                    <ALink href="#">FRH</ALink>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <span className="divider d-lg-show"></span>
+                        <ALink href="/pages/contact-us" className="contact d-lg-show ml-0"><i className="d-icon-map"></i>Contact</ALink>
+                        <ALink href="#" className="help d-lg-show"><i className="d-icon-info"></i> Need Help</ALink>
+                    </div>
+                </div>
+            </div> */}
+
+            <div className="header-top">
+                <div className="container">
+                    <div className="header-left">
+                        <p className="welcome-msg">Welcome to Jewelery</p>
+                    </div>
+                    <div className="header-right">
+                    <div className="dropdown">
                             <ALink href="#">USD</ALink>
                             <ul className="dropdown-box">
                                 <li><ALink href="#">USD</ALink></li>
@@ -52,7 +85,6 @@ export default function Header( props ) {
                                 </li>
                             </ul>
                         </div>
-
                         <span className="divider"></span>
                         <ALink href="/pages/contact-us" className="contact d-lg-show"><i className="d-icon-map"></i>Contact</ALink>
                         <ALink href="#" className="help d-lg-show"><i className="d-icon-info"></i> Need Help</ALink>
@@ -61,53 +93,64 @@ export default function Header( props ) {
                 </div>
             </div>
 
-            <div className="header-middle sticky-header fix-top sticky-content">
+            <div className="header-middle ">
                 <div className="container">
-                    <div className="header-left">
-                        <ALink href="#" className="mobile-menu-toggle" onClick={ showMobileMenu }>
+                    <div className="header-right">
+                        {/* <p className="welcome-msg">Welcome to Jewelery</p> */}
+                        <ALink href="#" className="mobile-menu-toggle" onClick={showMobileMenu}>
                             <i className="d-icon-bars2"></i>
                         </ALink>
+                    </div>
 
+                    <div className="header-center">
                         <ALink href="/" className="logo">
-                            <img src='./images/logo.png' alt="logo" width="153" height="44" />
+                            <img src='./images/logo.png' alt="logo" width="154" height="43" />
                         </ALink>
-
-                        <SearchBox />
                     </div>
 
                     <div className="header-right">
-                        <ALink href="tel:#" className="icon-box icon-box-side">
-                            <div className="icon-box-icon mr-0 mr-lg-2">
+                         {/* <ALink href="#" className="mobile-menu-toggle" onClick={showMobileMenu}>
+                            <i className="d-icon-bars2"></i>
+                        </ALink> */}
+
+                        <a href="tel:#" className="call icon-box icon-box-side">
+                            <div className="icon-box-icon text-white p-0">
                                 <i className="d-icon-phone"></i>
                             </div>
                             <div className="icon-box-content d-lg-show">
-                                <h4 className="icon-box-title">Call Us Now:</h4>
-                                <p>0(800) 123-456</p>
+                                <h4 className="icon-box-title text-white">Call Us Now:</h4>
+                                <p className="text-white">0(123) 456-789</p>
                             </div>
+                        </a>
+                        <ALink href="/pages/account" className="login-link mr-4 mr-lg-6">
+                            <i className="d-icon-user text-white"></i>
                         </ALink>
-                        <span className="divider"></span>
                         <ALink href="/pages/wishlist" className="wishlist">
-                            <i className="d-icon-heart"></i>
+                            <i className="d-icon-heart text-white"></i>
                         </ALink>
-                        <span className="divider"></span>
+                        <span className="divider mr-3 text-white"></span>
 
                         <CartMenu />
                     </div>
                 </div>
             </div>
 
-            <div className="header-bottom d-lg-show">
-                <div className="container">
+            {/* <div className={`header-bottom sticky-header fix-top sticky-content ${hasborder === 'true' ? 'no-border' : ''}`}> */}
+                {/* <div className="container">
                     <div className="header-left">
+                        <SearchBox />
+
+                    </div>
+
+                    <div className="header-center justify-content-center">
                         <MainMenu />
                     </div>
 
-                    <div className="header-right">
-                        <ALink href="#"><i className="d-icon-card"></i>Special Offers</ALink>
-                        <a href="https://d-themes.com/buynow/riodereact" className="ml-6">Buy Riode!</a>
+                    <div className="header-right mr-0">
+                        <a href="https://d-themes.com/buynow/riodereact" className="font-weight-semi-bold">Buy Riode!</a>
                     </div>
-                </div>
-            </div>
+                </div> */}
+            {/* </div> */}
         </header >
     );
 }
