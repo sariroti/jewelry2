a4933bd79cfb45d211cd4004a0ef047d916b50a4
budi erwanto <berwanto@eapexpert.com>
2024-12-03 18:31:21 +0700

    Fix Video sound and Category Text REsponsive

--- Jewelry/public/js/custom-video.js
+++ Jewelry/public/js/custom-video.js
@@ -1,6 +1,32 @@
 $(document).ready(function(){
    
+    
     $(window).scroll(function (event) {
-        $('#introSectionVideo1')[0].muted =false;
+       
+        if(($('#introSectionVideo1')[0].muted)){
+            
+          
+            $('#introSectionVideo1')[0].muted =false;
+        }
+        
+      
     });
+
+    setInterval(function(){
+        const videoObjectKeys = $('video');
+ 
+        Object.keys(videoObjectKeys).forEach((v, index) => {
+          const currentVideoObject = videoObjectKeys[v];
+       
+          if(currentVideoObject != null && v != 'length'){
+            
+              
+            if(currentVideoObject.src && currentVideoObject.src.indexOf('videos/First%20section_1.mp4') < 0){
+                console.log(currentVideoObject.src);
+                console.log(currentVideoObject);
+                currentVideoObject.muted = true;
+            }
+        }
+        });
+    },500);
 });
