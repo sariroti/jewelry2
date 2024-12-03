7b86f97611766aceacbda9e401d9ce2afee2f971
budi erwanto <berwanto@eapexpert.com>
2024-12-03 12:04:26 +0700

    Category Image Applied

--- Jewelry/components/partials/home/category-section-demo15.jsx
+++ Jewelry/components/partials/home/category-section-demo15.jsx
@@ -13,16 +13,16 @@ function CategorySection() {
                     style={ { backgroundColor: "#a6a6a6" } }>
                     <ALink href="/shop">
                         <figure className="category-media">
-                            <img src="/images/jewelry/Cat1.jpg" alt="banner" width="480" height="328" />
+                            <img src="/images/jewelry/kalung.jpg" alt="banner" width="480" height="328" />
                         </figure>
                     </ALink>
                     <div className="category-content y-50">
                         <Reveal keyframes={ fadeIn } triggerOnce>
-                            <h4 className="category-name font-weight-bold text-uppercase">New Arrivals</h4>
-                            <span className="category-count text-uppercase">
+                            <h4 className="category-name font-weight-bold text-uppercase">Necklace</h4>
+                            {/* <span className="category-count text-uppercase">
                                 <span>16</span> Products
-                        </span>
-                            <ALink href="/shop" className="btn btn-underline btn-link">Shop Now<i
+                        </span> */}
+                            <ALink href="/shop" className="btn btn-underline btn-link" style={{color:'white'}}>Shop Now<i
                                 className="d-icon-arrow-right"></i></ALink>
                         </Reveal>
                     </div>
@@ -31,16 +31,16 @@ function CategorySection() {
             <div className="col-lg-3 col-xs-6 order-lg-first">
                 <div className="intro-banner banner banner-fixed overlay-zoom" style={ { backgroundColor: "#3b3f42" } }>
                     <figure>
-                        <img src="/images/jewelry/Cat1.jpg" alt="banner" width="480" height="328" />
+                        <img src="/images/jewelry/cincin.jpg" alt="banner" width="480" height="328" />
                     </figure>
                     <div className="banner-content y-50 text-left">
                         <Reveal keyframes={ fadeIn } triggerOnce>
-                            <h4 className="banner-subtitle mb-2 text-primary text-uppercase">
+                            {/* <h4 className="banner-subtitle mb-2 text-primary text-uppercase">
                                 Footwear
-                        </h4>
-                            <h3 className="banner-title mb-2 ls-m text-white text-uppercase">New Design
+                        </h4> */}
+                            <h3 className="banner-title mb-2 ls-m text-white text-uppercase">Ring
                         </h3>
-                            <p className="mb-4 ls-m text-white">From $16.00</p>
+                            {/* <p className="mb-4 ls-m text-white">From $16.00</p> */}
                             <ALink href="/shop" className="btn btn-md btn-rounded btn-primary">Shop now<i
                                 className="d-icon-arrow-right"></i></ALink>
                         </Reveal>
@@ -50,16 +50,15 @@ function CategorySection() {
             <div className="col-lg-3 col-xs-6">
                 <div className="intro-banner banner banner-fixed overlay-zoom" style={ { backgroundColor: "#181b20" } }>
                     <figure>
-                        <img src="/images/jewelry/Cat1.jpg" alt="banner" width="480" height="328" />
+                        <img src="/images/jewelry/bross.jpg" alt="banner" width="480" height="328" />
                     </figure>
                     <div className="banner-content y-50 text-left">
                         <Reveal keyframes={ fadeIn } triggerOnce>
-                            <h4 className="banner-subtitle mb-2 text-primary text-uppercase">
+                            {/* <h4 className="banner-subtitle mb-2 text-primary text-uppercase">
                                 Big Sale
-                        </h4>
-                            <h3 className="banner-title mb-2 ls-m text-white text-uppercase">Mid
-                            Season's</h3>
-                            <p className="mb-4 ls-m text-white">Up to 70% Off</p>
+                        </h4> */}
+                            <h3 className="banner-title mb-2 ls-m text-white text-uppercase">Bross</h3>
+                            {/* <p className="mb-4 ls-m text-white">Up to 70% Off</p> */}
                             <ALink href="/shop" className="btn btn-md btn-rounded btn-primary">Shop now<i
                                 className="d-icon-arrow-right"></i></ALink>
                         </Reveal>
@@ -71,16 +70,16 @@ function CategorySection() {
                     style={ { backgroundColor: "#ceced0" } }>
                     <ALink href="/shop" >
                         <figure className="category-media">
-                            <img src="/images/jewelry/Cat1.jpg" alt="banner" width="480" height="328" />
+                            <img src="/images/jewelry/anting.jpg" alt="banner" width="480" height="328" />
                         </figure>
                     </ALink>
                     <div className="category-content y-50">
                         <Reveal keyframes={ fadeIn } triggerOnce>
-                            <h4 className="category-name font-weight-bold text-uppercase">Tops &amp; Jackets</h4>
-                            <span className="category-count text-uppercase">
+                            <h4 className="category-name font-weight-bold text-uppercase">Earing</h4>
+                            {/* <span className="category-count text-uppercase">
                                 <span>16</span> Products
-                        </span>
-                            <ALink href="/shop" className="btn btn-underline btn-link">Shop Now<i
+                        </span> */}
+                            <ALink href="/shop" className="btn btn-underline btn-link" style={{color:'white'}}>Shop Now<i
                                 className="d-icon-arrow-right"></i></ALink>
                         </Reveal>
                     </div>
